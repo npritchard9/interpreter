c9214669dab687b577bb33e43df961082c89ecea
Noah Pritchard <noahpritchard9@gmail.com>
2023-06-08 19:29:25 -0400

    builtin array first, last

--- src/builtins.rs
+++ src/builtins.rs
@@ -28,10 +28,46 @@ pub fn len(args: Vec<Object>) -> Object {
     }
 }
 
+pub fn first(args: Vec<Object>) -> Object {
+    if args.len() != 1 {
+        let msg = format!("wrong number of arguments. got={}, want=1", args.len());
+        return Object::Error(object::Err { msg });
+    }
+    match &args[0] {
+        Object::Array(a) => a.elements[0].clone(),
+        _ => {
+            let msg = format!(
+                "argument to 'first' must be array, got {}",
+                args[0].get_type()
+            );
+            Object::Error(object::Err { msg })
+        }
+    }
+}
+
+pub fn last(args: Vec<Object>) -> Object {
+    if args.len() != 1 {
+        let msg = format!("wrong number of arguments. got={}, want=1", args.len());
+        return Object::Error(object::Err { msg });
+    }
+    match &args[0] {
+        Object::Array(a) => a.elements.last().unwrap().clone(),
+        _ => {
+            let msg = format!(
+                "argument to 'last' must be array, got {}",
+                args[0].get_type()
+            );
+            Object::Error(object::Err { msg })
+        }
+    }
+}
+
 impl Builtins {
     pub fn new() -> Self {
         let mut h = HashMap::new();
         h.insert("len".to_string(), len as fn(Vec<Object>) -> Object);
+        h.insert("first".to_string(), first as fn(Vec<Object>) -> Object);
+        h.insert("last".to_string(), last as fn(Vec<Object>) -> Object);
         Builtins { fns: h }
     }
 }
